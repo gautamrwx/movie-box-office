65143636f5f0929653eebd11f92720c6d25ab779
gautamrwx <gautam.404@protonmail.com>
2021-03-04 00:25:03 +0530

    react routing pages created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred
+      </Route>
+
+      <Route>404 Page Not Found</Route>
+    </Switch>
+  );
 }
 
 export default App;
