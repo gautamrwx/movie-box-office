6454966f6bce6f9afa374bc6d1b2002470cad75c
gautamrwx <gautam.404@protonmail.com>
2021-03-05 17:05:08 +0530

    Finished Starred Page

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,7 +1,45 @@
-import React from 'react';
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { useShows } from '../misc/custom-hooks';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
 
 const Starred = () => {
-  return <div>This is starred</div>;
+  const [starred] = useShows();
+
+  const [shows, setshows] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+  const [error, seterror] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => apiGet(`/shows/${showId}`));
+
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setshows(results);
+          setisLoading(false);
+        })
+        .catch(err => {
+          seterror(err.message);
+          setisLoading(false);
+        });
+    } else {
+      setisLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {isLoading && <div>Shows Are Loading ...</div>}
+      {error && <div>Unknown Error Occourded</div>}
+      {!isLoading && !shows && <div>No Shows Were Starred</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Starred;
