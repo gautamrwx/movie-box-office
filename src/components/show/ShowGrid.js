f3aeb2b6efddd73d7f37909edade553adde6fb2f
gautamrwx <gautam.404@protonmail.com>
2021-03-05 16:19:51 +0530

    Added feature to Star or Unstar a Show

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import ShowCard from './ShowCard';
-
+import { useShows } from '../../misc/custom-hooks';
 import { FlexGrid } from '../Styled';
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 
 const ShowGrid = ({ data }) => {
+  const [starredShow, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
-        <ShowCard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-          summary={show.summary}
-        />
-      ))}
+      {data.map(({ show }) => {
+        const isStarred = starredShow.includes(show.id);
+
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
+
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+            summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
     </FlexGrid>
   );
 };
