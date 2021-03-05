0763d0eeffb0019a98723f2a3ed7981fed15eee2
gautamrwx <gautam.404@protonmail.com>
2021-03-05 17:32:26 +0530

    Created 2 hooks for input data persistance and Show card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,10 @@ import ActorGrid from '../components/Actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
 import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
+import { useLastQuery } from '../misc/custom-hooks';
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [results, setresults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
 
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,63 +1,16 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import Cast from '../components/show/Cast';
 import Details from '../components/show/Details';
 import Season from '../components/show/Season';
 import ShowMainData from '../components/show/ShowMainData';
-import { apiGet } from '../misc/config';
+import { useShow } from '../misc/custom-hooks';
 import { InfoBlock, ShowPageWrapper } from './Show.styled';
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case 'FETCH_SUCESS': {
-      return { isLoading: false, error: null, show: action.show };
-    }
-
-    case 'FETCH_FAILED': {
-      return { ...prevState, isLoading: false, error: action.error };
-    }
-
-    default:
-      return prevState;
-  }
-};
-
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
 
 const Show = () => {
   const { id } = useParams();
-
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  useEffect(() => {
-    let isMounted = true;
-
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(results => {
-        setTimeout(() => {
-          if (isMounted) {
-            dispatch({ type: 'FETCH_SUCESS', show: results });
-          }
-        }, 2000);
-      })
-      .catch(err => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_FAILED', error: err.message });
-        }
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [id]);
+  const { show, isLoading, error } = useShow(id);
 
   if (isLoading) {
     return <div>Data is being loaded..</div>;
