ae10e3ce436696e649072b87afabc197b350f463
gautamrwx <gautam.404@protonmail.com>
2021-03-05 13:53:53 +0530

    Usereducer used to handle fetch show state

---
+++ src/pages/Show.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
+const Show = () => {
+  const { id } = useParams();
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=episodes&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            dispatch({ type: 'FETCH_SUCESS', show: results });
+          }
+        }, 2000);
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
+
+  console.log('Show', show);
+
+  if (isLoading) {
+    return <div>Data is being loaded..</div>;
+  }
+
+  if (error) {
+    return <div>Error Occoured..</div>;
+  }
+
+  return <div>This is show page</div>;
+};
+
+export default Show;
